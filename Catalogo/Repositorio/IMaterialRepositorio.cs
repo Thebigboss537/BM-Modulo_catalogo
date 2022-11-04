658f97213b0ff7dfe69c2f59dced9e81cdede8ec
Eudes David Martinez Gonzalez <eudesdavid29@hotmail.com>
2022-11-04 17:36:44 -0500

    Agregar archivos de proyecto.

---
+++ Catalogo/Repositorio/IMaterialRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using Catalogo.Models.Dto;
+
+namespace Catalogo.Repositorio
+{
+    public interface IMaterialRepositorio
+    {
+        Task<List<MaterialDto>> GetMateriales();
+        Task<MaterialDto> GetMaterialById(int id);
+        Task<MaterialDto> CreateUpdate(MaterialDto MaterialDto);
+        Task<MaterialDto> AgregarArchivo(Material_archivoDto Material_archivoDto);
+        Task<bool> DeleteMaterial(int id);
+    }
+}
